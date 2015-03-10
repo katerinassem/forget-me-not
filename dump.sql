92ef9a85b7cd04f38dc83d94a1ed336e6262fa59
katerinassem <katarina.sem@mail.ru>
2015-03-10 12:46:58 +0300

    10March - dump

---
+++ dump.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE youcontact
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+CREATE  TABLE `youcontact`.`contact` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  `first_name` VARCHAR(30) NOT NULL,
+  `second_name` VARCHAR(30) NOT NULL,
+  `name_by_father` VARCHAR(30) NULL,
+  `date_of_birth` DATE NULL,
+  `sex` ENUM('m', 'f') NULL,
+  `sitizenship` VARCHAR(40) NULL,
+  `web_site` VARCHAR(100) NULL,
+  `email` VARCHAR(100) NULL,
+  `company` VARCHAR(50) NULL,
+  `photo_url` VARCHAR(100) NULL,
+  `address_id` INT(10) UNSIGNED NOT NULL,     
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE  TABLE `youcontact`.`address` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  `country` VARCHAR(40) NOT NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `street` VARCHAR(100) NULL,
+  `building` INT(10) UNSIGNED NULL,
+  `apartment` INT(10) UNSIGNED NULL,
+  `post_index` BIGINT(20) UNSIGNED NULL,
+  `contact_id` INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (`contact_id`)
+  REFERENCES `youcontact`.`contact` (`id`)
+  ON UPDATE NO ACTION
+  ON DELETE CASCADE,    
+  PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `youcontact`.`contact`
+ADD
+FOREIGN KEY (`address_id`)
+  REFERENCES `youcontact`.`address` (`id`)
+  ON DELETE SET NULL;
+
+CREATE  TABLE `youcontact`.`attachment` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  `file_name` VARCHAR(100) NOT NULL,
+  `unique_name` VARCHAR(200) NOT NULL,
+  `upload_date` DATETIME NOT NULL,  
+  `comment` VARCHAR(200) NULL,
+  `contact_id` INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (`contact_id`)
+  REFERENCES `youcontact`.`contact` (`id`)
+  ON UPDATE NO ACTION
+  ON DELETE CASCADE,    
+  PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE  TABLE `youcontact`.`telephone` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  `country_code` TINYINT UNSIGNED NOT NULL,
+  `operator_code` TINYINT UNSIGNED NOT NULL,
+  `telephone_number` BIGINT UNSIGNED NOT NULL,  
+  `telephone_type` ENUM('h', 'm') NOT NULL,
+  `comment` VARCHAR(200) NULL,
+  `contact_id` INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (`contact_id`)
+  REFERENCES `youcontact`.`contact` (`id`)
+  ON UPDATE NO ACTION
+  ON DELETE CASCADE,    
+  PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
